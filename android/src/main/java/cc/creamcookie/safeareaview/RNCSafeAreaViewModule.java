2409f69b0b76b9b2740540d8f9cc1a510012f726
Suhan Moon <lunyx@me.com>
2019-02-05 18:06:25 +0900

    Update RNCSafeAreaViewModule.java

--- android/src/main/java/cc/creamcookie/safeareaview/RNCSafeAreaViewModule.java
+++ android/src/main/java/cc/creamcookie/safeareaview/RNCSafeAreaViewModule.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.res.Configuration;
 import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Build;
 import android.util.DisplayMetrics;
 import android.view.*;
@@ -14,6 +15,7 @@ public class RNCSafeAreaViewModule extends ReactContextBaseJavaModule {
 
     private final ReactApplicationContext reactContext;
     private Activity mActivity;
+    private boolean isLight;
 
     public RNCSafeAreaViewModule(ReactApplicationContext reactContext) {
         super(reactContext);
@@ -29,8 +31,8 @@ public class RNCSafeAreaViewModule extends ReactContextBaseJavaModule {
             public void run() {
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                     Window w = mActivity.getWindow();
-                    w.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR | View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR);
                     w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+                    w.setStatusBarColor(Color.TRANSPARENT);
                 }
             }
         });
@@ -41,46 +43,77 @@ public class RNCSafeAreaViewModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void getPadding(Promise promise) {
+    public void setTintLight(final boolean isLight) {
+        this.isLight = isLight;
+        mActivity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    Window w = mActivity.getWindow();
+                    if (isLight) {
+                        w.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_VISIBLE);
+                    }
+                    else {
+                        w.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR | View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR);
+                    }
+                }
+            }
+        });
+    }
 
-        try {
-            final Window w = mActivity.getWindow();
-            mActivity.runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
+    @ReactMethod
+    public void getTintLight(final Promise promise) {
+        promise.resolve(this.isLight);
+    }
+
+    @ReactMethod
+    public void getPadding(final Promise promise) {
+
+        final Window w = mActivity.getWindow();
+        mActivity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+
+                try {
+                    boolean forceTop = false;
+
                     boolean isPhone = (reactContext.getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) < Configuration.SCREENLAYOUT_SIZE_LARGE;
                     int _o = reactContext.getResources().getConfiguration().orientation;
                     if (_o == Configuration.ORIENTATION_LANDSCAPE && isPhone) {
+                        forceTop = true;
                         w.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
                         w.clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
                     } else {
+                        forceTop = false;
                         w.addFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
                         w.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
                     }
+
+                    WritableMap map = Arguments.createMap();
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        WindowInsets insets = w.getDecorView().getRootWindowInsets();
+                        map.putDouble("top", forceTop ? 0 : pxToDp(insets.getStableInsetTop()));
+                        map.putDouble("left", pxToDp(insets.getStableInsetLeft()));
+                        map.putDouble("right", pxToDp(insets.getStableInsetRight()));
+                        map.putDouble("bottom", pxToDp(insets.getStableInsetBottom()));
+                    }
+                    else {
+                        map.putDouble("top", 0.0d);
+                        map.putDouble("left", 0.0d);
+                        map.putDouble("right", 0.0d);
+                        map.putDouble("bottom", 0.0d);
+                    }
+                    promise.resolve(map);
+
                 }
-            });
+                catch (Exception ex) {
+                    ex.printStackTrace();
+                    promise.reject(ex);
+                }
+            }
+        });
 
 
-            WritableMap map = Arguments.createMap();
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                WindowInsets insets = w.getDecorView().getRootWindowInsets();
-                map.putDouble("top", this.pxToDp(insets.getStableInsetTop()));
-                map.putDouble("left", this.pxToDp(insets.getStableInsetLeft()));
-                map.putDouble("right", this.pxToDp(insets.getStableInsetRight()));
-                map.putDouble("bottom", this.pxToDp(insets.getStableInsetBottom()));
-            }
-            else {
-                map.putDouble("top", 0.0d);
-                map.putDouble("left", 0.0d);
-                map.putDouble("right", 0.0d);
-                map.putDouble("bottom", 0.0d);
-            }
-            promise.resolve(map);
-        }
-        catch (Exception ex) {
-            ex.printStackTrace();
-            promise.reject(ex);
-        }
     }
 
     @Override
